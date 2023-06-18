45beb937c579f5fd04242ae24fcd0922f5455657
rafinic <rafinic@163.com>
2023-06-18 00:11:13 +0000

    add some definition about memory

--- include/memory/memory.h
+++ include/memory/memory.h
@@ -1,5 +1,17 @@
 #ifndef __header_memory__
 #define __header_memory__
 
+#define HalfWord_BitWidth (16)
+#define HalfWord_ByteWidth (HalfWord_BitWidth / 8)
+#define Word_BitWidth (32)
+#define Word_ByteWidth (Word_BitWidth / 8)
+#define DoubleWord_BitWidth (64)
+#define DoubleWord_ByteWidth (DoubleWord_BitWidth / 8)
+#define QuadWord_BitWidth (128)
+#define QuadWord_ByteWidth (QuadWord_BitWidth / 8)
+
+namespace MEMORY {
+
+}
 
 #endif
